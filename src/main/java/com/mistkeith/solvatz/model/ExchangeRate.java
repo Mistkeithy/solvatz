f528bebd44b6bd143de534b7574b9659f8a2c5ba
Mistkeithy <mail@mistkeith.com>
2022-12-24 20:18:46 +0600

    added ExchangeReate model

---
+++ src/main/java/com/mistkeith/solvatz/model/ExchangeRate.java
@@ -0,0 +1,48 @@
+package com.mistkeith.solvatz.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class ExchangeRate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "currency")
+    private String currency;
+
+    @Column(name = "rate")
+    private Double rate;
+
+    public ExchangeRate() {
+    }
+
+    public ExchangeRate(String currency, Double rate) {
+        this.currency = currency;
+        this.rate = rate;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public Double getRate() {
+        return rate;
+    }
+
+    public void setRate(Double rate) {
+        this.rate = rate;
+    }
+}
