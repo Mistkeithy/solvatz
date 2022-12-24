303f7b366f40aa995f432a74d51a4c17bca2df96
Mistkeithy <mail@mistkeith.com>
2022-12-24 17:10:57 +0600

    initializing

---
+++ src/main/java/com/mistkeith/solvatz/SolvatzApplication.java
@@ -0,0 +1,13 @@
+package com.mistkeith.solvatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolvatzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SolvatzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mistkeith/solvatz/SolvatzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mistkeith.solvatz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolvatzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
