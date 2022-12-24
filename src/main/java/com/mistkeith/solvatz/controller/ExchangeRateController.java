668a7cbc1182956a8387c92db2255702791ea30d
Mistkeithy <mail@mistkeith.com>
2022-12-24 21:52:41 +0600

    Added ExchangeRate Controller logic

--- src/main/java/com/mistkeith/solvatz/controller/ExchangeRateController.java
+++ src/main/java/com/mistkeith/solvatz/controller/ExchangeRateController.java
@@ -1,11 +1,23 @@
 package com.mistkeith.solvatz.controller;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mistkeith.solvatz.model.ExchangeRate;
+import com.mistkeith.solvatz.repository.ExchangeRateRepository;
+
+@RestController
 public class ExchangeRateController {
 
     @Autowired
@@ -35,13 +47,34 @@ public class ExchangeRateController {
     }
 
     private String sendRequest(String url) {
-        // Send a request to the specified URL and return the response
-        // Implementation details go here
+        String jsonResponse = "";
+        try {
+            // Send GET request to the specified URL
+            HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+            connection.setRequestMethod("GET");
+
+            // If the request was successful read the response and return
+            if (connection.getResponseCode() == 200) {
+                BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                jsonResponse = response.toString();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return jsonResponse;
     }
 
-    private double parseResponse(String jsonResponse) {
-        // Parse the JSON response from the API to get the exchange rate
-        // Implementation details go here
+    private double parseResponse(String jsonResponse) throws JSONException {
+        JSONObject jsonObject = new JSONObject(jsonResponse);
+        JSONObject ratesObject = jsonObject.getJSONObject("rates");
+        double exchangeRate = ratesObject.getDouble("KZT");
+        return exchangeRate;
     }
 
 }
